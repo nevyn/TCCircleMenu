8dba258c045bf51d3cbd1d2dcd3a5eb5367d5b8d
Joachim Bengtsson <joachimb@gmail.com>
2012-01-22 16:26:21 +0100

    Initial Commit

---
+++ TCCircleMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TCCircleMenu
+//
+//  Created by Joachim Bengtsson on 2012-01-22.
+//  Copyright (c) 2012 Third Cog Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
